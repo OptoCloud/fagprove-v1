4a5e796360cdb28f1b989e96ea2c7bd1a3e2397c
OptoCloud <26223094+OptoCloud@users.noreply.github.com>
2023-11-10 16:57:31 +0100

    Update frontend example page to be appropriate.
    The default template ships with this sentence and I didn't think this was approriate.

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,7 +2,7 @@
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-5">
-		<h1 class="h1">Let's get cracking bones!</h1>
+		<h1 class="h1">Welcome to SvelteKit + SkeletonUI!</h1>
 		<p>Start by exploring:</p>
 		<ul>
 			<li><code class="code">/src/routes/+layout.svelte</code> - barebones layout</li>
